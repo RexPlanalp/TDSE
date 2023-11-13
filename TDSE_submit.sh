a361fc176a69b642b9461ecaea5a7eb47652e5f2
PLANALP Donald Rex <dopl4670@photon15.colorado.edu>
2023-11-13 13:30:04 -0700

    Adjusted basis functions

---
+++ TDSE_submit.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+#SBATCH --job-name TDSE
+#SBATCH -p photons
+#SBATCH --nodes 1
+#SBATCH --ntasks 8
+#SBATCH --mem=32G 
+#SBATCH -o run_%j.log 
+#SBATCH -t 0-01:00:00
+#SBATCH --exclude=photon13
+
+REPO_DIR="/home/becker/dopl4670/TDSE/Solver"
+module purge                                                                                 
+module load intel/2019.0                                                                     
+module load openmpi                                                                          
+module load hdf5                                                                           
+module load cmake                                                                            
+module load lapack                                                                           
+module load blas                                                                             
+module load intel-gcc/6.3.0                                                                  
+
+hostname
+pwd
+
+mpiexec -n $SLURM_NTASKS python $REPO_DIR/TDSE.py >> results.log
+
+
