639f130b011cda15b97d8a4606f3a7eb9509a5d7
PLANALP Donald Rex <dopl4670@photon15.colorado.edu>
2023-11-14 12:25:25 -0700

    Modified gitignore

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-from petsc4py import get_include
-
-setup(
-    ext_modules=cythonize("kron.pyx", annotate=True),
-    include_dirs=[get_include()],
-)
