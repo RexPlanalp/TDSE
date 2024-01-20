0f896fda9c2215e5bf77b4492fc6b2b14a49225a
PLANALP Donald Rex <dopl4670@terra.colorado.edu>
2024-01-20 15:18:48 -0700

    Added submit scripts to repo

---
+++ submit_scripts/TDSE_submit.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+#SBATCH --job-name TDSE
+#SBATCH -p jila
+#SBATCH --nodes 1
+#SBATCH --ntasks 64
+#SBATCH --mem=144G
+#SBATCH -o run.log 
+#SBATCH -t 0-01:00:00
+
+
+REPO_DIR="/users/becker/dopl4670/Research/TDSE/Solver"
+                                                               
+hostname
+pwd
+
+mpiexec -n $SLURM_NTASKS python $REPO_DIR/TDSE.py >> results.log
+
+
+
+
