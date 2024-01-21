33408a9e11bd87c217a2f2095bedd11bf47021bd
PLANALP Donald Rex <dopl4670@terra.colorado.edu>
2024-01-21 02:50:26 -0700

    Added Files for plotting observables, Population: None

---
+++ Plotting/PAD_Plot.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import sys
+
+
+ANGULAR = "ANGULAR" in sys.argv
+RECT = "RECT" in sys.argv
+
+# Use an existing colormap
+base_cmap = plt.cm.viridis
+
+
+# Create a new colormap from the existing colormap
+# Start with fully transparent and gradually become opaque
+cmap_with_transparency = mcolors.LinearSegmentedColormap.from_list(
+    name='transparent_viridis',
+    colors=[(0, 0, 0, 0)] + [(c[0], c[1], c[2], i / 256) for i, c in enumerate(base_cmap(np.arange(256)))],
+    N=256
+)
+
+
+from matplotlib.colors import LinearSegmentedColormap
+
+# Define the colors for the colormap (white for near zero, black otherwise)
+colors = [(0, 0, 0), (1, 1, 1), (0, 0, 0)]  # Black -> White -> Black
+
+# Define the positions of the colors (0.5 is the midpoint)
+position = [0, 0.5, 1]
+
+# Create the custom colormap
+custom_cmap = LinearSegmentedColormap.from_list('custom_cmap', list(zip(position, colors)))
+
+PAD = np.load("PAD.npy")
+
+
+E_vals = PAD[0,:]
+theta_vals = PAD[1,:]
+phi_vals = PAD[2,:]
+pad_vals = PAD[3,:]
+
+pad_vals = np.real(pad_vals)
+x_vals = E_vals * np.sin(theta_vals) * np.cos(phi_vals)
+y_vals = E_vals * np.sin(theta_vals) * np.sin(theta_vals)
+z_vals = E_vals * np.cos(theta_vals)
+
+# Momentum Transform
+px_vals = np.sqrt(2*E_vals) * np.sin(theta_vals) * np.cos(phi_vals)
+py_vals = np.sqrt(2*E_vals) * np.sin(theta_vals) * np.sin(theta_vals)
+pz_vals = np.sqrt(2*E_vals) * np.cos(theta_vals)
+pad_momentum = pad_vals/np.sqrt(2*E_vals)
+
+
+
+if ANGULAR:
+    #plt.figure(figsize=(10, 5)) 
+    #plt.xlim([-1,1])
+    #plt.ylim([0,1])
+    plt.scatter(pz_vals,px_vals,c=pad_momentum, cmap="binary", norm=mcolors.LogNorm(vmin=10**(-11.13), vmax=10**(-5.13)))
+    plt.colorbar()
+    plt.savefig("PAD.png")
+
+if RECT:
+    plt.scatter(E_vals,theta_vals,c=pad_vals, cmap="binary", norm=mcolors.LogNorm(vmin=10**(-11.13), vmax=10**(-5.13)))
+    plt.savefig("E.png")
+
+    
+
