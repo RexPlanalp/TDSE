TISE/Hamiltonian: Fixed major race condition in matrix assembly, Analysis: Adjusted Energy Spectrum S_T calculation, needs futher work

Solver/Hamiltonian.py
@@ -21,7 +21,7 @@ class hamiltonian:
         return np.sqrt(((l+1)**2 - self.m**2)/((2*l+1)*(2*l+3)))
 
 
-    def H_MIX(self,basisInstance,gridInstance):
+    def H_MIX(self,basisInstance,gridInstance,tiseInstance):
         n_basis = basisInstance.n_basis
         order = basisInstance.order
         degree = basisInstance.degree
@@ -31,6 +31,7 @@ class hamiltonian:
 
         def H_mix_R_element(x,i,j):
             return basis_funcs[i](x)*basis_funcs[j](x,1)
+        
 
         H_mix_lm = PETSc.Mat().createAIJ([self.lmax+1,self.lmax+1],comm = comm,nnz = 2)
         H_mix_lm.setUp()
@@ -61,15 +62,20 @@ class hamiltonian:
         return None
 
 
-    def H_ANG(self,basisInstance,gridInstance):
+    def H_ANG(self,basisInstance,gridInstance,tiseInstance):
         n_basis = basisInstance.n_basis
         order = basisInstance.order
         degree = basisInstance.degree
         basis_funcs = basisInstance.basis_funcs
         dt = gridInstance.dt
 
-        def H_ang_R_element(x,i,j):
-            return basis_funcs[i](x)*basis_funcs[j](x)/ np.sqrt(x**2 + 1E-25)
+        if tiseInstance.ECS:
+            def H_ang_R_element(x,i,j):
+                return basis_funcs[i](x)*basis_funcs[j](x)*tiseInstance.q(x)/ np.sqrt(tiseInstance.R(x)**2 + 1E-25) 
+        else:
+            def H_ang_R_element(x,i,j):
+                return basis_funcs[i](x)*basis_funcs[j](x)/ np.sqrt(x**2 + 1E-25)
+            
 
         H_ang_lm = PETSc.Mat().createAIJ([self.lmax+1,self.lmax+1],comm = comm,nnz = 2)
         istart,iend = H_ang_lm.getOwnershipRange()
@@ -79,6 +85,7 @@ class hamiltonian:
                     H_ang_lm.setValue(i,j,-(i)*self.clm(i-1,self.m))
                 elif j == i+1:
                     H_ang_lm.setValue(i,j,(j)*self.clm(j-1,self.m))
+        comm.barrier()
         H_ang_lm.assemble()
 
         H_ang_R =  PETSc.Mat().createAIJ([n_basis,n_basis],comm = comm,nnz = 2*degree+1)
@@ -88,6 +95,7 @@ class hamiltonian:
             for j in range(n_basis):
                     H_element = basisInstance.integrate(H_ang_R_element,i,j)
                     H_ang_R.setValue(i,j,H_element)
+        comm.barrier()
         H_ang_R.assemble()
 
         total = kronV5(H_ang_lm,H_ang_R,2*(2*order+1))
@@ -119,6 +127,7 @@ class hamiltonian:
                     H_length_lm.setValue(i,j,self.clm(i-1,self.m))
                 elif j == i+1:
                     H_length_lm.setValue(i,j,self.clm(j-1,self.m))
+        comm.barrier()
         H_length_lm.assemble()
 
         H_length_R = PETSc.Mat().createAIJ([n_basis,n_basis],comm = comm,nnz = 2*degree+1)
@@ -128,6 +137,7 @@ class hamiltonian:
             for j in range(n_basis):
                         H_element = basisInstance.integrate(H_length_R_element,i,j)
                         H_length_R.setValue(i,j,H_element)
+        comm.barrier()
         H_length_R.assemble()
 
         total = kronV4(H_length_lm,H_length_R,2*(2*order+1))
@@ -187,7 +197,7 @@ class hamiltonian:
             column_indices = column_indices.astype("int32")
         
             H_atom.setValues(i,column_indices,values)
-
+        comm.barrier()
         H_atom.assemble()
         viewer = PETSc.Viewer().createBinary("matrix_files/H_0.bin","w")
         H_atom.view(viewer)
@@ -216,9 +226,10 @@ class hamiltonian:
         istart,iend = I.getOwnershipRange()
         for i in range(istart,iend):
             I.setValue(i,i,1)
+        comm.barrier()
         I.assemble()
 
-        total = kronV4(I,S_R,(2*order+1))
+        total = kronV5(I,S_R,(2*order+1))
 
         I.destroy()
         self.S = total

Solver/TISE.py
@@ -16,22 +16,37 @@ class tise:
         self.nmax = input_par["lm"]["nmax"]
         self.lmax = input_par["lm"]["lmax"]
 
-        CAP = input_par["box"]["CAP"]
-        if CAP:
-            self.region = input_par["box"]["region"]
-            self.cap = True
 
-            self.R0 = int(self.region*input_par["box"]["xmax"])
-            self.eta = input_par["box"]["eta"]
-            self.n = input_par["box"]["n"]
-        else:
-            self.cap = False
+        self.ECS = input_par["box"]["ECS"]
+        self.region = input_par["box"]["region"]
+        self.R0 = int(self.region*input_par["box"]["xmax"])
+        self.potential = input_par["box"]["pot"]
+        if comm.rank == 0:
+            if self.ECS:
+                print(f"Applying ECS Boundary at{self.R0}")
+
+        if self.potential == "H":
+            def H(x):
+                return (-1/np.sqrt(x**2 + 1E-25))
+            self.pot_func = H
+        elif self.potential == "He":
+            def He(x):
+                return (-1/np.sqrt(x**2 + 1E-25)) + -1.0*np.exp(-2.0329*x)/np.sqrt(x**2 + 1E-25)  - 0.3953*np.exp(-6.1805*x)
+            self.pot_func = He
+
+        
+        
             
         
 
     def _createH_l(self,basisInstance,l):
-        def _H_element(x,i,j):
-            return basis_funcs[i](x) * (-1/2) * basis_funcs[j](x,2) + basis_funcs[i](x) * basis_funcs[j](x) * l*(l+1)/(2*np.sqrt(x**4 + 1E-25 )) + basis_funcs[i](x) * basis_funcs[j](x)* (-1/np.sqrt(x**2 + 1E-25))
+
+        if self.ECS:
+            def _H_element(x,i,j):
+                return basis_funcs[i](x,1) * (1/2) * basis_funcs[j](x,1)/self.q(x) + basis_funcs[i](x) * basis_funcs[j](x) * l*(l+1)/(2*np.sqrt(self.R(x)**4 + 1E-25 ))*self.q(x) + basis_funcs[i](x) * basis_funcs[j](x)* self.pot_func(self.R(x))*self.q(x)
+        else:
+            def _H_element(x,i,j):
+                return basis_funcs[i](x,1) * (1/2) * basis_funcs[j](x,1) + basis_funcs[i](x) * basis_funcs[j](x) * l*(l+1)/(2*np.sqrt(x**4 + 1E-25 )) + basis_funcs[i](x) * basis_funcs[j](x)* self.pot_func(x)
 
         n_basis = basisInstance.n_basis
         basis_funcs = basisInstance.basis_funcs
@@ -50,7 +65,8 @@ class tise:
                     if H_element == 0:
                         continue
 
-                    FFH_R.setValue(i,j,H_element)      
+                    FFH_R.setValue(i,j,H_element)  
+        comm.barrier()    
         FFH_R.assemble()
         self.FFH_R_list.append(FFH_R)
         return None
@@ -86,8 +102,7 @@ class tise:
         n_basis,_ = self.S_R.getSize()
         ViewHDF5 = PETSc.Viewer().createHDF5("Hydrogen.h5", mode=PETSc.Viewer.Mode.WRITE, comm= PETSc.COMM_WORLD)
             
-        #if (self.lmax >= self.nmax):
-            #self.nmax = self.lmax +1
+        
         
 
         
@@ -102,6 +117,10 @@ class tise:
                 num_of_energies = self.nmax - i
             
             H = self.FFH_R_list[i]
+            #if comm.rank == 0:
+                #print(f"Solving System for l = {l}")
+                #test1,test2 = H.getRow(0)
+                #print(len(test1),len(test2))
             
 
             E = SLEPc.EPS().create()
@@ -172,36 +191,15 @@ class tise:
         ViewHDF5.destroy()    
         return None
     
-    def addComplexPot(self,basisInstance,gridInstance):
-        if not self.cap:
-            return
-        basis_funcs = basisInstance.basis_funcs
-        n_basis = basisInstance.n_basis
-        order = basisInstance.order
+    def R(self,x):
+        x_copy = np.copy(x).astype("complex")
+        mask = x>self.R0
+        x_copy[mask] = x_copy[mask] * np.exp(1j*np.pi/4)
+        return x_copy
 
-        xmax = gridInstance.rmax
-        if comm.rank == 0:
-            print("Adding Complex Potential")
-        def _polyCAP(x):
-            pot = np.log(1-np.cos((np.pi/2)*(1-x/(xmax))))
-            return 1j*pot
-        def _linearCAP(x):
-            return -1j*0.1*(x/xmax)
-        def _tanhCAP(x):
-            return -100j * (1+np.tanh((x-xmax)))
-        def _H_CAP(x,i,j):
-            return basis_funcs[i](x) * basis_funcs[j](x) * _tanhCAP(x)
-        H_CAP = PETSc.Mat().createAIJ([n_basis,n_basis],comm = PETSc.COMM_WORLD,nnz = 2*order +1)
-        rowstart,rowend = H_CAP.getOwnershipRange()
-        for i in range(rowstart,rowend):
-            for j in range(n_basis):
-                    H_cap_element = basisInstance.integrate(_H_CAP,i,j)
-                    if H_cap_element == 0:
-                        continue
-                    H_CAP.setValue(i,j,H_cap_element)      
-        H_CAP.assemble()
+    def q(self,x):
+        ones = np.ones_like(x)
+        mask = x>self.R0
+        ones[mask] = ones[mask] * np.exp(1j*np.pi/4)
+        return ones
 
-        for l in range(self.lmax+1):
-            FFH_R_l = self.FFH_R_list[l]
-            FFH_R_l.axpy(1,H_CAP)
-        return 