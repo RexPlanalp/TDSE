394b1aaff7151b120fa896317f8f343c808bc75b
PLANALP Donald Rex <dopl4670@terra.colorado.edu>
2024-06-28 09:53:08 -0600

    Preparing to merge/reset with master

--- Plotting/PAD_Plot.py
+++ Plotting/PAD_Plot.py
@@ -45,14 +45,14 @@ pz_vals = np.sqrt(2*E_vals) * np.cos(theta_vals)
 pad_momentum = pad_vals/np.sqrt(2*E_vals)
 
 max_mom = np.max(np.real(pad_momentum))
-min_mom = np.max(np.real(pad_momentum))*10**-6
+min_mom = np.max(np.real(pad_momentum))*10**-2
 
 max_E = np.max(pad_vals)
 min_E = np.max(pad_vals)*10**-2
 
 if ANGULAR:
-    #plt.scatter(px_vals, py_vals, c=pad_momentum, cmap="hot_r")
-    plt.scatter(px_vals, pz_vals, c=pad_momentum, cmap="hot_r",norm=mcolors.LogNorm(vmin=min_mom,vmax=max_mom))
+    plt.scatter(px_vals, py_vals, c=pad_momentum, cmap="hot_r")
+    #plt.scatter(px_vals, py_vals, c=pad_momentum, cmap="hot_r",norm=mcolors.LogNorm(vmin=min_mom,vmax=max_mom))
     plt.gca().set_aspect('equal', adjustable='box')
     plt.colorbar()
     plt.savefig("images/PAD.png")
@@ -95,8 +95,6 @@ if ASYM:
     plt.clf()
 
     plt.scatter(E_vals,phi_vals,c=asymmetry_vals, cmap="bwr",vmin = -1,vmax = 1)
-    plt.xlim([0,0.7349968763])
-    plt.axvline(0.192)
     plt.colorbar()
     plt.savefig("images/A_rect.png")
 
