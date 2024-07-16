c62eecfe37cb51efdc5479330b04abe875fbd971
PLANALP Donald Rex <dopl4670@terra.colorado.edu>
2024-07-16 14:19:30 -0600

    Added functionality for linear as well as log plot

--- Plotting/PES_Plot.py
+++ Plotting/PES_Plot.py
@@ -6,8 +6,16 @@ import matplotlib.pyplot as plt
 E_range = np.load("PES_files/E.npy")
 PES = np.load("PES_files/PES.npy")
 
+plt.figure()
 plt.semilogy(E_range,np.real(PES),color = "k")
 plt.xlabel("Energy (au)")
 plt.ylabel("Yield (Log scale)")
+plt.savefig("images/PES_log.png")
+plt.clf()
 
+plt.figure()
+plt.plot(E_range,np.real(PES),color = "k")
+plt.xlabel("Energy (au)")
+plt.ylabel("Yield")
 plt.savefig("images/PES.png")
+plt.clf()
